107c435c03433d566e568f6f5b06455b1e61a212
Dena Moshfegh <dmoshfegh@dmo.local>
2016-04-23 13:15:25 +0200

    add moodle comments to all file

--- studentquiz.js
+++ studentquiz.js
@@ -1,3 +1,26 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * TODO
+ *
+ * @package    mod_studentquiz
+ * @copyright  2016 HSR (http://www.hsr.ch)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
 $(document).ready(function() {
     $('.studentquiz_behaviour input[type="radio"]').on('click', function() {
         $.post('../../question/behaviour/studentquiz/save.php', { save: 'vote', questionid: $(this).attr('name').substr(1), rate: $(this).val() });
--- version.php
+++ version.php
@@ -1,14 +1,42 @@
 <?php
-    defined('MOODLE_INTERNAL') || die();
-    $plugin->component = 'qbehaviour_studentquiz';
-    $plugin->version      = 2016042300;
-    $plugin->release      = 'v1.0.0';
-    $plugin->requires     = 2015051100; // 2.9
-    $plugin->dependencies = array(
-        'qbehaviour_immediatefeedback' => 2015111600,
-        'mod_studentquiz' => ANY_VERSION
-    );
-    $plugin->maturity     = MATURITY_STABLE;
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines the version and other meta-info about the plugin
+ *
+ * Setting the $plugin->version to 0 prevents the plugin from being installed.
+ * See https://docs.moodle.org/dev/version.php for more info.
+ *
+ * @package    mod_studentquiz
+ * @copyright  2016 HSR (http://www.hsr.ch)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+_
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'qbehaviour_studentquiz';
+$plugin->version      = 2016042300;
+$plugin->release      = 'v1.0.0';
+$plugin->requires     = 2015051100; // 2.9
+$plugin->dependencies = array(
+    'qbehaviour_immediatefeedback' => 2015111600,
+    'mod_studentquiz' => ANY_VERSION
+);
+$plugin->maturity     = MATURITY_STABLE;
 
 
 
