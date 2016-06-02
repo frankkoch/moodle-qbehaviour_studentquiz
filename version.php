e9765b72e3332bca913400571e99d6d33088ea96
Dena Moshfegh <dmoshfegh@dmo.local>
2016-06-02 17:09:58 +0200

    update version

--- version.php
+++ version.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -20,7 +21,7 @@
  * Setting the $plugin->version to 0 prevents the plugin from being installed.
  * See https://docs.moodle.org/dev/version.php for more info.
  *
- * @package    qbehaviour_studentquiz
+ * @package    mod_studentquiz
  * @copyright  2016 HSR (http://www.hsr.ch)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -28,14 +29,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_studentquiz';
-$plugin->version      = 2016052900;
-$plugin->release      = 'v1.0.0';
-$plugin->requires     = 2015111600; // Version 3.0.
+$plugin->version      = 2016052903;
+$plugin->release      = 'v1.0.3';
+$plugin->requires     = 2015051106; // 3.0
 $plugin->dependencies = array(
     'qbehaviour_immediatefeedback' => 2015111600,
     'mod_studentquiz' => ANY_VERSION
 );
 $plugin->maturity     = MATURITY_STABLE;
-
-
-
